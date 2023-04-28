d640c7379a0c10d79ac330af8609a7f6ace1a1e3
Arunavo Biswas <65866038+DuesExMachina@users.noreply.github.com>
2023-04-28 12:11:16 +0530

    Update SubstrWithUniqueChars.cpp

--- SubstrWithUniqueChars.cpp
+++ SubstrWithUniqueChars.cpp
@@ -1,5 +1,22 @@
 //CPP program to search all substrings with unique characters
 
+//Algorithm
+/*
+1.Input the string s
+2.take n= s.length() 
+3. n will be our window size
+4. Now a run a loop which will start with window size n and decrease to 1
+   and will decrease the size of n/window size after every iteration
+   for(i=n;i!=1;i--)
+5. for each iteration of above loop run another loop
+6. this loop will find every substring of n window size
+7. run this loop for every n from 0 to n-1-i(last array position minus current window size) 
+   for(j=0;j<n-i;j++)
+8. and to find substring of s starting at index j and of size i
+   s.substring(j,i);
+9. Add a function check() to check if all chars are unique or not in the substring
+*/
+
 
 #include <iostream>
 #include "string"
