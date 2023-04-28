d85f9758dbcbd1a6cbb00c4ea3a8c791d6dba36b
Arunavo Biswas <65866038+DuesExMachina@users.noreply.github.com>
2023-04-28 12:28:45 +0530

    Update SubstrWithUniqueChars.cpp

--- SubstrWithUniqueChars.cpp
+++ SubstrWithUniqueChars.cpp
@@ -42,7 +42,7 @@ int main()
     int n=s.length();
     
     for(int i=n;i!=1;i--){
-        for(int j=0;j<n-i;j++){
+        for(int j=0;j<=n-i;j++){
             if(check(s.substr(j,i))){
             cout<<s.substr(j,i)<<"\t";}
         }
