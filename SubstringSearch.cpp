645590ba0f2842ce67c68ac953619bf4f9b9323b
Arunavo Biswas <65866038+DuesExMachina@users.noreply.github.com>
2023-04-28 12:21:29 +0530

    Update SubstringSearch.cpp

--- SubstringSearch.cpp
+++ SubstringSearch.cpp
@@ -28,7 +28,7 @@ int main()
     int n=s.length();
     
     for(int i=n;i!=1;i--){
-        for(int j=0;j<n-i;j++){
+        for(int j=0;j<=n-i;j++){
             cout<<s.substr(j,i)<<"\t";
         }
     }
