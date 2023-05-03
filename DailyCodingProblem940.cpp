1e141091745106a754f94d8aba6789d477dacd9c
Arunavo Biswas <65866038+DuesExMachina@users.noreply.github.com>
2023-05-03 15:52:27 +0530

    Create DailyCodingProblem940.cpp

---
+++ DailyCodingProblem940.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+class vertex{
+  public:
+  int a;
+  int b;
+  int t;
+};
+class node{
+    public:
+    node(int N){
+        int reach[N];
+    }
+}
+int main()
+{
+   vertex v[10]={{0, 1, 5},
+    {0, 2, 3},
+    {1, 3, 8},
+    {2, 3, 1},
+    {3, 5, 10},
+    {3, 4, 5}
+};
+
+
+
+    return 0;
+}
